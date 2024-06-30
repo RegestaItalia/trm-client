4bda35883ef972ab1907f368438870db05bcdb7d
simonegaffurini <simone.gaffurini@regestaitalia.it>
2024-06-30 14:00:37 +0200

    fix cli update message

--- src/utils/checkCliUpdate.ts
+++ src/utils/checkCliUpdate.ts
@@ -1,6 +1,6 @@
 import { Logger } from 'trm-core';
 import * as getLatestVersion from 'get-latest-version';
-import { diff } from "semver";
+import { diff, gt } from "semver";
 import { getClientVersion } from './getClientVersion';
 
 export async function checkCliUpdate() {
@@ -8,7 +8,7 @@ export async function checkCliUpdate() {
         const latestVersion = await getLatestVersion.default('trm-client');
         const localVersion = getClientVersion();
         const versionDiff = diff(localVersion, latestVersion);
-        if (versionDiff === 'minor' || versionDiff === 'major') {
+        if ((versionDiff === 'minor' || versionDiff === 'major') && gt(latestVersion, localVersion)) {
             Logger.warning(`A newer version of trm-client (v${latestVersion}) is available.`);
             Logger.warning(`New versions can introduce features and bug fixes.`);
             Logger.warning(`It is recommended to update your client by running the command`);
