67747e464fb3f253c1e28f8bcc48896aeac17b2e
Simone Gaffurini <simone.gaffurini@regestaitalia.it>
2026-03-10 21:52:16 +0100

    7.5.1

--- src/command/implementations/FindDependencies.ts
+++ src/command/implementations/FindDependencies.ts
@@ -38,7 +38,7 @@ export class FindDependencies extends AbstractCommand {
                 text: `Customer packages (${custPackageDependencies.length})`,
                 children: custPackageDependencies.map(o => {
                     return {
-                        text: `${o.abapPackage.devclass} (${o.entries.length})`,
+                        text: `${o.abapPackage.devclass} (${o.entries.reduce((sum, entry) => sum + entry.dependency.length, 0)})`,
                         children: o.entries.map(t => {
                             return {
                                 text: t.tableName,
@@ -67,7 +67,7 @@ export class FindDependencies extends AbstractCommand {
                 text: `SAP packages (${sapPackageDependencies.length})`,
                 children: sapPackageDependencies.map(o => {
                     return {
-                        text: `${o.abapPackage} (${o.entries.length})`,
+                        text: `${o.abapPackage} (${o.entries.reduce((sum, entry) => sum + entry.dependency.length, 0)})`,
                         children: o.entries.map(t => {
                             return {
                                 text: chalk.underline(t.tableName),
